6d2e6f8d31c8bc880525b4ca5dbfa79877a1c441
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2025-04-04 17:26:34 +0530

    Sign up Auth using firebase

---
+++ Blogging Website/register.js
@@ -0,0 +1,50 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCspsRZ2zjlIQApf3R460pA0srURis_UPo",
+    authDomain: "bloggingloginpage.firebaseapp.com",
+    projectId: "bloggingloginpage",
+    storageBucket: "bloggingloginpage.firebasestorage.app",
+    messagingSenderId: "979404700608",
+    appId: "1:979404700608:web:9cb2d00342539a30fc6625"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// submit button
+const submit = document.getElementById('signin');
+
+submit.addEventListener("click", function (event) {
+    event.preventDefault()
+    // alert("Please Fill Details First");
+
+    //  inputs
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+
+            alert("Creating Account....");
+            window.location.href = "newuser.html";
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage)
+            // ..
+        });
+})
